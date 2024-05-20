d97383d77daedb03c507a960ceda8bf4d12374ba
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2024-05-20 10:47:08 +0700

    memperbaiki error

--- RelasiDanPewarisan/RelasiDanPewarisan.cpp
+++ RelasiDanPewarisan/RelasiDanPewarisan.cpp
@@ -5,23 +5,25 @@
 using namespace std;
 
 class mahasiswa {
-private: 
+private:
 	const int id;
 	string nama;
 	float nilai;
 
 public:
-	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai){}
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {}
+
+
+	~mahasiswa() {
+		cout << "Id    = " << id << endl;
+		cout << "Nama  = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+	}
+
 };
 
-~mahasiswa() {
-	cout << "Id    = " << id << endl;
-	cout << "Nama  = " << nama << endl;
-	cout << "Nilai = " << nilai << endl;
-}
-
 int main() {
-	mahasiswa mhs(12, "Asroni", 90, 5);
+	mahasiswa mhs(12, "Asroni", 90.5);
 
 	return 0;
 }
